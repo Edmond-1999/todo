5789e181ee5090921a99eadfd04bda0e288080d6
Edmond-1999 <agburugaedmond@gmail.com>
2026-08-25 13:24:35 +0100

    commit message

---
+++ schemas/todo.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class TodoBase(BaseModel):
+    title: str
+    description: str
+    completed: bool
+
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoUpdate(TodoBase):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+
+class Todo(TodoBase):
+    id: int
